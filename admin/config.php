3815fdd0ad251649bf35c5af2a2872968070f80f
NoizeJo <noizejo@mail.ru>
2017-02-12 12:38:59 +0200

    initial commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ocstore/admin/');
+define('HTTP_CATALOG', 'http://ocstore/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ocstore/admin/');
+define('HTTPS_CATALOG', 'http://ocstore/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/Soft/OpenServer/domains/OcStore/admin/');
+define('DIR_SYSTEM', 'E:/Soft/OpenServer/domains/OcStore/system/');
+define('DIR_IMAGE', 'E:/Soft/OpenServer/domains/OcStore/image/');
+define('DIR_LANGUAGE', 'E:/Soft/OpenServer/domains/OcStore/admin/language/');
+define('DIR_TEMPLATE', 'E:/Soft/OpenServer/domains/OcStore/admin/view/template/');
+define('DIR_CONFIG', 'E:/Soft/OpenServer/domains/OcStore/system/config/');
+define('DIR_CACHE', 'E:/Soft/OpenServer/domains/OcStore/system/storage/cache/');
+define('DIR_DOWNLOAD', 'E:/Soft/OpenServer/domains/OcStore/system/storage/download/');
+define('DIR_LOGS', 'E:/Soft/OpenServer/domains/OcStore/system/storage/logs/');
+define('DIR_MODIFICATION', 'E:/Soft/OpenServer/domains/OcStore/system/storage/modification/');
+define('DIR_UPLOAD', 'E:/Soft/OpenServer/domains/OcStore/system/storage/upload/');
+define('DIR_CATALOG', 'E:/Soft/OpenServer/domains/OcStore/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'MySQL');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
